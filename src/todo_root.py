54a4bb1e23ac3f1dfde6a94b7f549b5363133f3f
Tyler Petresky <tnpetresky@gmail.com>
2017-01-17 01:22:20 -0500

    Allow 'init' subcommand to be used. Take note of __del__ in TodoRoot.

--- src/todo_root.py
+++ src/todo_root.py
@@ -1,19 +1,54 @@
 from src.yaml import YAML
-from os.path import expanduser
 from src.adapters.yaml_adapter import YamlAdapter
+from src.todo_list import TodoList
 
 class TodoRoot:
     _config = YAML.load('./config.yml')
     _lists = []
+    _changed = False
 
     def __init__(self):
         # Load the list(s) into memory
         self._adapter = self._choose_adapter()
-        self._lists = [self._adapter.load_list('testlist')]
+        self._lists = self._adapter.load_all_lists()
+
+    # def __del__(self):
+        # If there are any changes to be saved, save them
+        # This code doesn't work. Due to a change in Python3,
+        # you can no longer use open() to open files in the __del__
+        # function. So, this need to be fixed.
+        # For now, these two lines will be placed in each function
+        # individually.
+        # if self._changed:
+            # self._adapter.store_lists(self._lists)
 
     def init(self, args):
         print('INITIALIZE ->', args.list)
         print(args)
+
+        # Check if list exists with given name
+        already_exists = False
+        for list in self._lists:
+            if list.is_named(args.list):
+                already_exists = True
+                break
+        if already_exists:
+            print('List with name \'' + args.list + '\' already exists.')
+            return
+
+        # Template YAML for making a new list
+        raw = '''
+            'name': {name}
+            'tasks': []
+        '''.format(name=args.list)
+
+        # The new list is only added into memory.
+        # Will be saved by storage adapters later
+        self._lists.append(TodoList(YAML.parse_yaml(raw)))
+        self._changed = True
+
+        ## Temporary; see __del__
+        self._adapter.store_lists(self._lists)
 
     def add(self, args):
         print('ADD TASK ->', args.list, ':', args.task)

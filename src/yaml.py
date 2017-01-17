21cdc5bebf2e2a2b945695ad11d87e164788bd91
Tyler Petresky <tnpetresky@gmail.com>
2017-01-17 01:19:38 -0500

    YAML static class can parse YAML into objects and now write to files as well

--- src/yaml.py
+++ src/yaml.py
@@ -1,12 +1,23 @@
 import yaml
 from os.path import expanduser
 
+def fix_filepath(filepath):
+    # If the file is absolute, expand the path
+    if filepath[0] == '~':
+        filepath = expanduser('~') + filepath[1:]
+    return filepath
+
 class YAML:
+    @staticmethod
+    def parse_yaml(raw):
+        return yaml.load(raw)
+
     @staticmethod
     def load(filepath):
         print('Loading file \'' + filepath + '\'')
+        return yaml.load(open(fix_filepath(filepath), 'r'))
 
-        # If the file is absolute, expand the path
-        if filepath[0] == '~':
-            filepath = expanduser('~') + filepath[1:]
-        return yaml.load(open(filepath, 'r'))
+    @staticmethod
+    def write(filepath, data):
+        print('Writing file \'' + filepath + '\'', data)
+        yaml.dump(data, open(fix_filepath(filepath), 'w'))

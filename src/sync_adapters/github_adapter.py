7450c045caa01302a227c2e55fc2419eb20d5b58
Tyler Petresky <tnpetresky@gmail.com>
2017-01-18 18:02:43 -0500

    Add basis of sync adapters

---
+++ src/sync_adapters/github_adapter.py
@@ -0,0 +1,6 @@
+class GithubAdapter(SyncAdapter):
+    def upload(self):
+        print('Uploading to Github')
+
+    def sync(self):
+        print('Syncing from Github')
---
+++ src/sync_adapters/sync_adapter.py
@@ -0,0 +1,6 @@
+class SyncAdapter:
+    def upload(self):
+        print('Uploading to sync location(s)')
+
+    def sync(self):
+        print('Downloading data from sync location(s)')

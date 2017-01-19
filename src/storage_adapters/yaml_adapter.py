657e3b017962601f5e4328c7efd886c8b2c2df86
Tyler Petresky <tnpetresky@gmail.com>
2017-01-18 19:11:16 -0500

    Added documentation to the YAML adapter

--- src/storage_adapters/yaml_adapter.py
+++ src/storage_adapters/yaml_adapter.py
@@ -3,7 +3,13 @@ from src.storage_adapters.storage_adapter import StorageAdapter
 from src.todo_list import TodoList
 from src.utils.logger import Logger
 
+# @class YamlAdapter
+# @desc YAML-based storage adapter to allow the program
+# to interact with YAML files for storage and retrieval.
 class YamlAdapter(StorageAdapter):
+    # Load all of the lists that are present in the
+    # lists.yml file.
+    # @return Array of all of the loaded lists
     def load_all_lists(self):
         Logger.debug('Loading YAML lists')
         lists_file = YAML.load('~/.todo/lists.yml')
@@ -13,9 +19,14 @@ class YamlAdapter(StorageAdapter):
             lists.append(self.load_list(list))
         return lists
 
-    def load_list(self, list):
-        return TodoList(YAML.load('~/.todo/' + list + '.yml'))
+    # Load a single list with a given name.
+    # @param list_name Name of the list to load into memory
+    # @return TodoList object of the loaded list
+    def load_list(self, list_name):
+        return TodoList(YAML.load('~/.todo/' + list_name + '.yml'))
 
+    # Write all passed-in lists to their respective files.
+    # @param lists Array of the lists to write
     def store_lists(self, lists):
         Logger.debug('Storing YAML lists')
         list_names = []
@@ -25,5 +36,7 @@ class YamlAdapter(StorageAdapter):
 
         YAML.write('~/.todo/lists.yml', list_names)
 
+    # Write a single, given list to its respective file.
+    # @param list TodoList object to write
     def store_list(self, list):
         YAML.write('~/.todo/' + list._name + '.yml', list.package_data())
